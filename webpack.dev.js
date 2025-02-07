4f7f0b4c43ad73f53e691d30d377eb0b4ad0f887
gusxz <85022129+Donutelo@users.noreply.github.com>
2025-02-07 12:36:40 -0300

    Fiz um template de HTML e tirei o título das propriedades do webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,9 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "development",
-    devServer: {
-        watchFiles: ["./src/template.html"],
-        title: "Production",
-      },
+  mode: "development",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
 });
